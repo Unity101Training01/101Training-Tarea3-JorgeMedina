19b56536a00ed8ec5bd11f5f630832221f027516
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-11-16 20:04:06 -0300

    Seeker enemy added

---
+++ Assets/Scripts/SeekerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
